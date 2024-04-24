a62b244edcdef230696eeabd4a7b80ffa74b32ff
sabbir1257 <masabbir1257@gmail.com>
2024-04-25 01:34:12 +0600

    comment Change

--- js/variable.js
+++ js/variable.js
@@ -4,7 +4,7 @@ var parson = "sabbir";
 var location = "mirpur 1";
 var special = "parul akter";
 
-// Number type variable 
+// numeric type variable 
 var age = 10;
 var price = 24;
 var rating = 10;

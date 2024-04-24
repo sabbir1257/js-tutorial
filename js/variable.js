6a69939f54e626aaf371bdabd506dc436f277fa4
sabbir1257 <masabbir1257@gmail.com>
2024-04-25 01:06:52 +0600

    string type variable code  push

--- js/variable.js
+++ js/variable.js
@@ -1,3 +1,10 @@
+// string type variable 
+
 var parson = "sabbir";
 var location = "mirpur 1";
 var special = "parul akter";
+
+// Number type variable 
+var age = 10;
+var price = 24;
+var rating = 10;

7ee5e74ea8fc6aab395f20a86062e76a8bbc702d
sabbir1257 <masabbir1257@gmail.com>
2024-05-01 00:35:39 +0600

    variable name Declare rules 4 push

--- js/variable-name.js
+++ js/variable-name.js
@@ -1,18 +1,29 @@
 // good variable 
 var price = 10;
 
+
 // 1. varible name can not be any keywords
 
 // Demo......
 var false = 30;
 var return = true;
 
+
 // 2. variable name has to be in one word . No space
 
 // Demo......
 var my home address = "New California";
 
+
 // 3. variable name can not have quotaion 
 
 // Demo......
 var "name" = "Tom Hanks";
+
+
+// 4. variable nama can not starts with a number but can ends with a number 
+
+// Demo......
+var 99Club = 1967;
+// good variable 
+var club99 = 1957;

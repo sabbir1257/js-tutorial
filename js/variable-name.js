5774a2ba18b096ba5f8658e78cfce6d1b5d6b1a5
sabbir1257 <masabbir1257@gmail.com>
2024-05-01 00:26:53 +0600

    variable name Declare rules 2 push

---
+++ js/variable-name.js
@@ -0,0 +1,10 @@
+// 1. varible name can not be any keywords 
+
+// Demo......
+// var false = 30;
+// var return = true;
+
+// 2. variable name has to be in one word . No space 
+
+// Demo......
+// var my home address = "New California";

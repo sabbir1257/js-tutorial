4047327444bb4dbdffc9fb95dd9ba21d7c2e8e0f
sabbir1257 <masabbir1257@gmail.com>
2024-05-03 01:55:59 +0600

    shorthand js file push

--- js/operation.js
+++ js/operation.js
@@ -1,8 +1,24 @@
 var onionPrice = 20;
 var eggPrice = 10;
+// addition ,  subtraction
 var totalPrice = onionPrice + eggPrice;
-console.log(totalPrice); 
+var priceDifference = onionPrice - eggPrice;
 
+// console.log(priceDifference);
+// console.log(totalPrice);
 // console.log(onionPrice);
 // console.log(eggPrice);
 // console.log(onionPrice + eggPrice);
+
+// multplication
+var orangePrice = 20;
+var quantiy = 7;
+var totalCost = orangePrice * quantiy;
+// console.log(totalCost);
+
+
+// division 
+var mony = 500;
+var player = 10;
+var eachPlayer = mony / player;
+console.log(eachPlayer);
---
+++ js/shorthand.js
@@ -0,0 +1,35 @@
+// variable declaration 
+var tomatoPrice = 20;
+// variable value update 
+tomatoPrice = 25;
+
+// add something to the previous value 
+var price1 = 31;
+var price2 = 10;
+
+// set a new value
+// price1 = 35 ;
+
+// add some value to the existhing value 
+price1 = price1 + 10;
+// console.log(price1);
+
+// addititon to the same variable shorthand
+// += means adding some value to the existing value
+price1 += 10;
+
+price2 = price2 - 5;
+// shorthand 
+price2 -= 5;
+
+
+var age = 15;
+age = 1 + 1;
+// ++ means adding 1 to the existing 
+age++;
+
+
+var love = 100;
+love = love - 1;
+// -- means you are deducting 1 from the existing value 
+love--;

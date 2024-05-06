49117adaab1ac38701d9fd6280a38df66948f9f6
sabbir1257 <masabbir1257@gmail.com>
2024-05-07 00:05:28 +0600

    convert string to number

--- js/conversion.js
+++ js/conversion.js
@@ -1,4 +1,19 @@
-var price1 = "31" ; 
-var price2 = "10" ;
-var totalPrice = price1 + price2 ;
-console.log(totalPrice);
+var price1 = "31";
+var price2 = "10";
+var totalPrice = price1 + price2;
+// console.log(totalPrice);
+
+var price1 = 31;
+var price2 = "10";
+var totalPrice = price1 + price2;
+// console.log(totalPrice);
+
+var price1 = "31";
+var price2 = 10;
+var totalPrice = price1 + price2;
+// console.log(totalPrice);
+
+// convert string to unmber 
+console.log(price1);
+var price1Number = parseInt(price1)
+console.log(price1Number);

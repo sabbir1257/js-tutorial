b2b6b155d7d27d6d00c7ddd7f928df63719310fc
sabbir1257 <masabbir1257@gmail.com>
2024-05-07 01:13:33 +0600

    convert string to parsefloat

--- js/conversion.js
+++ js/conversion.js
@@ -17,3 +17,7 @@ var totalPrice = price1 + price2;
 console.log(price1);
 var price1Number = parseInt(price1)
 console.log(price1Number);
+
+var gpa = "3.41";
+var gpaNumber = parseFloat(gpa);
+console.log(gpaNumber);

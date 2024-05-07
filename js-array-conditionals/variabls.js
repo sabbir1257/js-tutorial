1b39fc4ae47e5de64b500a997457233b01ff873d
sabbir1257 <masabbir1257@gmail.com>
2024-05-08 01:49:23 +0600

    best practice

---
+++ js-array-conditionals/variabls.js
@@ -0,0 +1,22 @@
+// number type variable 
+var sunglassPrice = 120;
+
+// string type variable 
+var specialOneName = 'Ariana Grande';
+
+// boolean type variable 
+var isSingle = false ;
+
+// two type of number: Integer:120,
+// float, decimal: 3.65,
+
+// variable naming convention: rules, best practice 
+/**
+ * 1. single word 
+ * 2. mo gap or speace 
+ * 3. no quite 
+ * 4. no keyword 
+ * 5. can not start with a number 
+ * 6. can not contain dash 
+ * 7. should use camelCase 
+ **/

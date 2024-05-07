d7ee3ad1bca46d6dc926dbaef5b707c3f9384e07
sabbir1257 <masabbir1257@gmail.com>
2024-05-08 01:02:05 +0600

    practice 2 solve

--- js/practice.js
+++ js/practice.js
@@ -1,6 +1,11 @@
+// practice 1 
 var ma = 1000 ;
 var apple = 300 ;
 var orange = 400 ;
 var foodTotalPrice = apple + orange ;
 var lessFoodPrice = ma - foodTotalPrice ;
-console.log(lessFoodPrice); 
+// console.log(lessFoodPrice); 
+
+// practice 2 
+var a = 119 / 5;
+console.log(a);

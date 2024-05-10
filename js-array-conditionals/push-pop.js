9a4ca706b6774aed3347207f0257eae3c0bff570
sabbir1257 <masabbir1257@gmail.com>
2024-05-11 00:45:59 +0600

    se pop to get last element

--- js-array-conditionals/push-pop.js
+++ js-array-conditionals/push-pop.js
@@ -1,7 +1,19 @@
 var numbers = [78, 45, 98, 45];
-numbers.push(63);
+// use push to add element to an array as the last element array 
+// numbers.push(63);
 // console.log(numbers);
 var friends = ['saymon' , 'usman' , 'emon' , 'fahim'];
 friends.push('shakil')
 friends.push('raidul')
-console.log(friends);
+// console.log(friends);
+
+
+// // use pop to get last element 
+// console.log(numbers);
+// // numbers.pop();
+// var element = numbers.pop();
+// console.log(numbers);
+// console.log(element);
+
+numbers.pop(1)
+console.log()

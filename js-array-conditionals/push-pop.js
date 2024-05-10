cb9223bda78ac3d037f65aba7a6a1ffe03933587
sabbir1257 <masabbir1257@gmail.com>
2024-05-11 00:21:34 +0600

    push method system push

---
+++ js-array-conditionals/push-pop.js
@@ -0,0 +1,7 @@
+var numbers = [78, 45, 98, 45];
+numbers.push(63);
+// console.log(numbers);
+var friends = ['saymon' , 'usman' , 'emon' , 'fahim'];
+friends.push('shakil')
+friends.push('raidul')
+console.log(friends);

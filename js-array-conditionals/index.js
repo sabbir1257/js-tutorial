b245a4cd3f0257351dbe3c35f768e6004bee6948
sabbir1257 <masabbir1257@gmail.com>
2024-05-10 23:40:32 +0600

    index array 3 no step  push

--- js-array-conditionals/index.js
+++ js-array-conditionals/index.js
@@ -1,7 +1,13 @@
 var numbers = [45, 68, 78, 56, 89, 98]
 
-// get element by index
+// 1. get element by index
 var element = numbers[1]; 
-console.log(element);
+// console.log(element);
+
+// 2. set element value by index 
 numbers[1] = 77;
-console.log(numbers);
+// console.log(numbers);
+
+// 3. find index of an element 
+var positionIndex = numbers.indexOf(89);
+console.log(positionIndex);

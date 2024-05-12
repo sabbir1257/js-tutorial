f513a16098f465821287743af76fe04e121c4ab0
sabbir1257 <masabbir1257@gmail.com>
2024-05-13 02:02:44 +0600

    push

---
+++ js-array-conditionals/multiple-conditions.js
@@ -0,0 +1,16 @@
+var isGraduated = true;
+var salary = 75000;
+
+// if (isGraduated == true) {
+//      console.log('Eso biye kore feli');
+// }
+// else {
+//      console.log('tor kopale Biya nai');
+// }
+
+if (isGraduated == true && salary > 50000) {
+     console.log('Eso biye kore feli');
+}
+else {
+     console.log('tor kopale Biya nai');
+}

e21ddd901c81d8e6f6e370ec0b7a020236d9aab8
sabbir1257 <masabbir1257@gmail.com>
2024-05-13 02:22:25 +0600

    4nd Multiple Sortho push

--- js-array-conditionals/multiple-conditions.js
+++ js-array-conditionals/multiple-conditions.js
@@ -1,6 +1,6 @@
-var isGraduated = true;
-var salary = 75000;
-var cars = 1;
+// var isGraduated = true;
+// var salary = 75000;
+// var cars = 1;
 
 // if (isGraduated == true) {
 //      console.log('Eso biye kore feli');
@@ -16,9 +16,21 @@ var cars = 1;
 //      console.log('tor kopale Biya nai');
 // }
 
-if (isGraduated == true && salary > 50000 && cars >= 1) {
-     console.log('Eso biye kore feli');
+// if (isGraduated == true && salary > 50000 && cars >= 1) {
+//      console.log('Eso biye kore feli');
+// }
+// else {
+//      console.log('tor kopale Biya nai');
+// }
+
+
+var isGraduated = true;
+var salary = 5000;
+var cars = 1;
+
+if (isGraduated === true || salary > 50000 || cars >= 1) {
+     console.log('Eso prem kori');
 }
-else {
-     console.log('tor kopale Biya nai');
+else{
+     console.log('tomar friend er mobile number dao');
 }

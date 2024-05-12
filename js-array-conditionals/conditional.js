40ff537e082a60a4c426cbfd33f5161177b9e6c2
sabbir1257 <masabbir1257@gmail.com>
2024-05-13 00:58:56 +0600

    var if section push

---
+++ js-array-conditionals/conditional.js
@@ -0,0 +1,27 @@
+var iphonePrice = 79000 ;
+var myBudget = 95000;
+// if iphone price is less than my budget . I will buy the iphone 
+
+/**
+ * if iphone price is less than my budget 
+ *      i will buy the iphone 
+ * 
+ * 
+ * if(condition) {
+ *    //will execute if the condition is true 
+ * }
+ * 
+ * **/ 
+
+if (iphonePrice < myBudget) {
+     console.log('iphoen kine futani marbo');
+}
+
+var chickenPrice = 500;
+var myMoney = 50;
+if(chickenPrice <= myMoney){
+     console.log('Murgir raan khamu and haddii chibamu');
+}
+if(chickenPrice > myMoney){
+     console.log('Smashed potato and lentils soup');
+}

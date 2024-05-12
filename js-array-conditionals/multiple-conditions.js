f1b806c07d29c865c375bc9f522f4b24eb65fbaa
sabbir1257 <masabbir1257@gmail.com>
2024-05-13 02:06:10 +0600

    3nd Sortho push

--- js-array-conditionals/multiple-conditions.js
+++ js-array-conditionals/multiple-conditions.js
@@ -1,5 +1,6 @@
 var isGraduated = true;
 var salary = 75000;
+var cars = 1;
 
 // if (isGraduated == true) {
 //      console.log('Eso biye kore feli');
@@ -8,7 +9,14 @@ var salary = 75000;
 //      console.log('tor kopale Biya nai');
 // }
 
-if (isGraduated == true && salary > 50000) {
+// if (isGraduated == true && salary > 50000) {
+//      console.log('Eso biye kore feli');
+// }
+// else {
+//      console.log('tor kopale Biya nai');
+// }
+
+if (isGraduated == true && salary > 50000 && cars >= 1) {
      console.log('Eso biye kore feli');
 }
 else {

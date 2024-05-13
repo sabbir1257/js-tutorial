76f1f9fbed7733bdab9911f8cf10c8769032c549
sabbir1257 <masabbir1257@gmail.com>
2024-05-14 01:09:29 +0600

    multi stage conditions push

---
+++ js-array-conditionals/multi-stage-conditions.js
@@ -0,0 +1,16 @@
+var money = 10;
+var danishPrice = 45 ;
+var butterBread = 35 ;
+var toastBiscute = 20;
+if (danishPrice < money) {
+     console.log('Dan Dan danish khamu');
+}
+else if (butterBread < money) {
+     console.log('Butter bon khamu');
+}
+else if (toastBiscute < money) {
+     console.log('chubai chubai toast biscuit khamu');
+}
+else{
+     console.log('Khali cha e sasther jonno valo');
+}

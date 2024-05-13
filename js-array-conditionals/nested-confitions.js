51d769686da96b4046cd0d320efbb660cbe521ed
sabbir1257 <masabbir1257@gmail.com>
2024-05-14 01:18:34 +0600

    nested confitions push

---
+++ js-array-conditionals/nested-confitions.js
@@ -0,0 +1,21 @@
+var math = true;
+var geometry = true;
+var straightLine = false;
+
+if (math == true) {
+     if (geometry == true) {
+          if (straightLine == true) {
+               
+          }
+          else{
+               console.log('baka pothe cholba na');
+          }
+     }
+     else{
+          console.log('pithagorous hotte parba na ');
+     }
+}
+
+else{
+
+}

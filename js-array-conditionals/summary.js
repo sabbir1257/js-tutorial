be97b6f543cb353b43ae45c8979ce8cd19b5f16c
sabbir1257 <masabbir1257@gmail.com>
2024-05-14 01:43:55 +0600

    summary section push

---
+++ js-array-conditionals/summary.js
@@ -0,0 +1,14 @@
+var destinations = ['sajek', 'coxsBazar', 'nepal', 'bali', 'SP'];
+
+// console.log(destinations[2]);
+
+destinations[2] = 'thailand';
+
+var index = destinations.indexOf('bali')
+console.log(index);
+
+// add a new array in destinations 
+destinations.push('bhutan');
+
+// remove array in destinations
+destinations.pop();

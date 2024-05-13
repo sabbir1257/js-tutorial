33acdbb5e111968039bdc53ca4b3a175204065c2
sabbir1257 <masabbir1257@gmail.com>
2024-05-14 03:56:28 +0600

    while loop section push

---
+++ loops-practice-problems/revision.js
@@ -0,0 +1,30 @@
+// string typw variable
+var bottle = 'water';
+var bottleColor = 'yellow';
+var panColor = 'blue';
+
+// number type variable
+var sunglassPrice = 120;
+
+// Boolean type variable 
+var isExpensivre = true;
+
+var tableItems = ['bottle' , 'pan' , 'mouse' , 'sunglass' , 'paper'];
+
+var item3 = tableItems[3];
+tableItems[4] = 'notebook';
+var penIndex = tableItems.indexOf('pen');
+// push , pop
+
+
+// conditionals
+
+if (tableItems.length > 4) {
+     
+}
+else if (tableItems[3] == 'pan') {
+     
+}
+else{
+
+}
---
+++ loops-practice-problems/while.js
@@ -0,0 +1,10 @@
+var roastGiven = 0;
+
+// roastGiven = roastGiven + 1;
+// roastGiven += 1;
+
+while (roastGiven < 7) {
+     console.log('Roast Den, Please !! ');
+     console.log(roastGiven);
+     roastGiven++;
+}

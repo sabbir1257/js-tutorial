25610f869192cf5faaef803e84e8560ec36c5b6b
sabbir1257 <masabbir1257@gmail.com>
2024-05-16 22:47:57 +0600

    while loop Declare rules section push

--- loops-practice-problems/while.js
+++ loops-practice-problems/while.js
@@ -3,8 +3,17 @@ var roastGiven = 0;
 // roastGiven = roastGiven + 1;
 // roastGiven += 1;
 
+
+/*
+1. Loop variable 
+2. condition inside while 
+3. Loop body 
+4. Change the loop variable 
+*/ 
+
+
 while (roastGiven < 7) {
      console.log('Roast Den, Please !! ');
      console.log(roastGiven);
      roastGiven++;
-}
+};

b209f87f62263ec2c87af9a24b5d73786b7be58a
sabbir1257 <masabbir1257@gmail.com>
2024-05-17 00:21:03 +0600

    for loop all code push

--- loops-practice-problems/for.js
+++ loops-practice-problems/for.js
@@ -1,21 +1,28 @@
 // while loop 
 
-// var roastGiven = 0;
-// while (roastGiven < 7) {
-//      console.log('Roast Den, Please !!');
-//      roastGiven++;
-// }
+/* var roastGiven = 0;
+while (roastGiven < 7) {
+     console.log('Roast Den, Please !!');
+     roastGiven++;
+} */
 
 
 // for loop 
 
-for(var roastGiven = 0 ; roastGiven < 7 ; roastGiven++){
+/* for(var roastGiven = 0 ; roastGiven < 7 ; roastGiven++){
      console.log('Roast Den, Please');
-} 
+} */ 
 
 
 // simple varsion of for loop 
 
-for(i = 0 ; i < 7 ; i++){
+/* for(i = 0 ; i < 7 ; i++){
+     console.log(i);
+} */
+
+
+// i = i + 2 ;
+// i += 2 ;
+for(var i = 0; i < 10 ; i+=2){
      console.log(i);
 }

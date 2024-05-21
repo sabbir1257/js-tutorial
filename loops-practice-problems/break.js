20f17170b5002bb36e39eeb6f4f01d95fbe500a9
sabbir1257 <masabbir1257@gmail.com>
2024-05-22 00:42:33 +0600

    break file and break use rules push

---
+++ loops-practice-problems/break.js
@@ -0,0 +1,13 @@
+/* for (var i = 1; i <= 20; i++) {
+     console.log(i);
+     if (i > 10) {
+          break;
+     }
+} */
+
+
+var roastGiven = 0;
+while (roastGiven < 10) {
+     console.log('Roast Den, gift item ansi');
+     roastGiven++;
+}

f19a6651563950a221f100dbc4ed3c1cf392037e
sabbir1257 <masabbir1257@gmail.com>
2024-05-23 01:51:47 +0600

    array update file push

---
+++ loops-practice-problems/array-update.js
@@ -0,0 +1,8 @@
+var fruits = ['Apple' , 'Banana', 'Orange'];
+var bananaIndex = fruits.indexOf('Banana');
+// fruits[index] = "orange";
+fruits[bananaIndex] = "Mango"
+console.log(fruits);
+fruits.pop()
+fruits.push('Watermelon')
+console.log(fruits);

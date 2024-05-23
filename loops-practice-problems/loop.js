85908cb812399a900ab88e101dfea1a3352eca70
sabbir1257 <masabbir1257@gmail.com>
2024-05-24 00:45:56 +0600

    display string for 39 times

---
+++ loops-practice-problems/loop.js
@@ -0,0 +1,17 @@
+/* 
+--------------------------
+LOOP
+---------------------------
+1. Display "Ajke amar mon valo nei" for 39 times
+*/
+
+
+// 1 solve 
+var loop = "Ajke amar mon valo nei";
+var i = 0;
+while (i <= 39) {
+     // console.log(loop);
+     i++;
+}
+
+

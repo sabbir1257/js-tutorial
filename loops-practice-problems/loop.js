9e9433f403e486a10b479782a05b6b2e18685741
sabbir1257 <masabbir1257@gmail.com>
2024-05-24 00:52:50 +0600

    show all odd numbers 581 to 623 solve and push

--- loops-practice-problems/loop.js
+++ loops-practice-problems/loop.js
@@ -4,7 +4,9 @@ LOOP
 ---------------------------
 1. Display "Ajke amar mon valo nei" for 39 times
 2. Display numbers between 58 to 98
-
+3. Show all even numbers from 412 to 456
+4. Show all odd numbers 581 to 623 
+5. Difference between while loop and for loop 
 */
 
 
@@ -16,4 +18,19 @@ while (i <= 39) {
      i++;
 }
 
+
+// 3 solve
+var even = 412;
+while(even < 456){
+     // console.log(even);
+     even+=2
+}
+
+
+// 4 solve
+var odd = 581;
+while (odd < 623) {
+     console.log(odd);
+     odd+=2
+}
 

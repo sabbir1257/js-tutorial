a088e9ff86247a8512233f0f6a3472ceb513c27b
sabbir1257 <masabbir1257@gmail.com>
2024-05-26 01:16:45 +0600

    function file push

---
+++ js-function-object/function.js
@@ -0,0 +1,9 @@
+// function declaration
+function startFan() {
+     console.log('Stand up');
+     console.log('walk towards the switch');
+     console.log('Press the switch');
+} 
+
+// call the function 
+startFan();

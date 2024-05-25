c5ebfd47bb1ebfef361eb66fa4f37a16400cfeb7
sabbir1257 <masabbir1257@gmail.com>
2024-05-26 02:27:42 +0600

    parameter file push

---
+++ js-function-object/parameter.js
@@ -0,0 +1,21 @@
+function bringSingara(mony) {
+     console.log('taka disen:', mony);
+     console.log('ai nen singara');
+}
+
+var taka = 300;
+// bringSingara(taka); 
+
+function add(num1, num2) {
+     console.log('going to add:', num1, num2);
+}
+
+// add(125 , 955);
+
+
+function sum(a, b, c, d, e) {
+     var sum = a + b + c + d + e;
+     console.log(sum);
+}
+
+sum(10, 23, 34, 432, 423, 21);

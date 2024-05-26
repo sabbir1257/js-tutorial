c23a9f1e99e288cdccce1e0c647609959de0e0fb
sabbir1257 <masabbir1257@gmail.com>
2024-05-27 02:09:15 +0600

    object file push

---
+++ js-function-object/object.js
@@ -0,0 +1,13 @@
+var student = {
+     id: 115,
+     name: 'Sakib Sarker',
+     class: 9,
+     marks: 98
+}
+
+var mobile = {
+     brand: 'Samsung',
+     price: 19000,
+     storage: '64gb',
+     camera: '7MP'
+}

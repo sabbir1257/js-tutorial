01af44af1b0a9aa166ac16ee0be64a6ee225fd8e
sabbir1257 <masabbir1257@gmail.com>
2024-05-28 02:16:52 +0600

    switch file push

---
+++ js-function-object/switch.js
@@ -0,0 +1,42 @@
+const color = 'yellow';
+if (color === 'green') {
+     console.log('You are a green friend');
+}
+else if (color === 'blue') {
+     console.log('You are a blue friend');
+}
+else if (color === 'red') {
+     console.log('You are a red friend');
+}
+else if (color === 'white') {
+     console.log('You are a white friend');
+}
+else if (color === 'yellow') {
+     console.log('You are a yellow friend');
+}
+
+else {
+     console.log('You are a black friend');
+}
+
+
+// switch
+switch (color) {
+     case 'yellow':
+          console.log('You are a yellow friend');
+          break
+     case 'blue':
+          console.log('You are a blue friend');
+          break
+     case 'red':
+          console.log('You are a red friend');
+          break
+     case 'white':
+          console.log('You are a white friend');
+          break
+     case 'green':
+          console.log('You are a green frined');
+          break
+     default: 
+     console.log('You are a black friend');
+}

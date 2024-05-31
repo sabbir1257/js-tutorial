911412fd1099cd7580559f0b1a083bdd2f74110c
sabbir1257 <masabbir1257@gmail.com>
2024-06-01 01:31:17 +0600

    miles to kilometer function file push

---
+++ apply-js-core-concepts/milesToKm.js
@@ -0,0 +1,8 @@
+function milesTokilometer(miles) {
+     const kilometer = miles * 1.60;
+     return kilometer;
+}
+
+const office = 12;
+const convartKilometer =  milesTokilometer(office);
+console.log(convartKilometer);

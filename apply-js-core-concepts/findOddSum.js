eda91cc2867495d25a65769d482f95e1c119f1ae
sabbir1257 <masabbir1257@gmail.com>
2024-06-04 02:14:15 +0600

    find odd and sum file push

---
+++ apply-js-core-concepts/findOddSum.js
@@ -0,0 +1,13 @@
+function getSumOfAnArray(numbers) {
+     let sum = 0;
+     for (let i = 0; i < numbers.length; i++) {
+          const index = i;
+          const element = numbers[i];
+          sum = sum + element;
+          console.log(index, element, sum);
+     }
+     return sum;
+}
+
+const myNumber = [12, 65, 45, 78, 32, 45, 61];
+getSumOfAnArray(myNumber);

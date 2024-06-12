aea43bc941760bdc58cc6738f0083f641d967a9a
sabbir1257 <masabbir1257@gmail.com>
2024-06-12 09:48:00 +0600

    max3 file fush

---
+++ js-string-problems-practice/max3.js
@@ -0,0 +1,15 @@
+const jim = 69;
+const dela = 97;
+const chinku = 99;
+
+if (jim > dela && jim > chinku) {
+     console.log('Jim will get the cake');
+}
+else if(dela > jim && dela > chinku){
+     console.log('Dela will get the cake');
+}
+else{
+     console.log('Chinku will get the cake');
+}
+
+Math.max(55, 78);

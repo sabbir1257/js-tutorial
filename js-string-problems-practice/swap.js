afdaed71295930df2b5f6b81035938a769686f50
sabbir1257 <masabbir1257@gmail.com>
2024-06-12 09:29:00 +0600

    max file push

--- js-string-problems-practice/swap.js
+++ js-string-problems-practice/swap.js
@@ -1,9 +1,16 @@
 let first = 5;
 let second = 7;
 console.log(first, second);
+/* this is wrong approach */
 // second = first;
 // first = second;
-const temp = first;
-first = second;
-second = temp; 
+
+/* approach: 1 */
+// const temp = first; 
+// first = second;
+// second = temp; 
+
+/* approach: 2 ---  Destructuring */
+[first , second] = [second , first];
+
 console.log( first, second );

2d62601116dd73d1f8b6043317fd4fb705428cd6
sabbir1257 <masabbir1257@gmail.com>
2024-11-05 14:57:57 +0600

    Block scope global scope simple understanding of Hoisting

---
+++ comon-js-concepts/hoisting.js
@@ -0,0 +1,15 @@
+print5();
+prient10();
+
+for (let i = 0; i < 5; i++) {
+     console.log(i);
+}
+
+
+function print5(){
+     console.log('inside print5',5);
+}
+
+const prient10 = function(){
+     console.log('inside print10', 10);
+}
---
+++ comon-js-concepts/scope.js
@@ -0,0 +1,19 @@
+function add(a, b) {
+     const total = a + b;
+     console.log(total);
+     if (b > 5) {
+          const sum = 25 + a + b;
+     }
+     else {
+          const sum = 5 + a + b;
+          var current = sum;
+     }
+     console.log(current);
+     
+     return total;
+};
+
+// console.log(a, b);
+
+
+add(5, 7)

27e61e26697d42ee6443dd4ccfcfbbef4e49aa1f
sabbir1257 <masabbir1257@gmail.com>
2024-11-05 14:03:31 +0600

    double equal (==) vs triple equal (===), implicit conversion

---
+++ comon-js-concepts/triple.js
@@ -0,0 +1,17 @@
+// always use ===
+// equal comparison doesnt work for non-primitive 
+// 
+
+
+const first = 2;
+const second = '2';
+if(first == second){
+     console.log('values are equal');
+}
+else{
+     console.log('valuse are not equal');
+} 
+
+// type coercion
+// type conversion
+// type casting

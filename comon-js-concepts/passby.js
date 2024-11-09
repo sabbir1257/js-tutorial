a9fc25d604ca2f253c2d8e3474e0789b13473f61
sabbir1257 <masabbir1257@gmail.com>
2024-11-10 00:02:53 +0600

    code modify and code push

--- comon-js-concepts/passby.js
+++ comon-js-concepts/passby.js
@@ -1,3 +1,4 @@
+// primitive types are pass by value 
 let num1 = 5;
 let num2 = 7;
 
@@ -7,10 +8,12 @@ function multiply(a, b) {
      return result;
 };
 
-// console.log(num1);
+console.log(num1);
 const output = multiply(num1, num2);
-// console.log(output);
+console.log(num1);
 
+
+// object and array are pass by reference
 let student1 = {name: 'Jalil', partner: 'Borsha'};
 let student2 = {name: 'raj', partner: 'anika'}
 

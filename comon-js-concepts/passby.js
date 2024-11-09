566cec530e6f3f82243651464231f5dfd1a41372
sabbir1257 <masabbir1257@gmail.com>
2024-11-10 00:00:39 +0600

    function arguments pass by reference pass by value

---
+++ comon-js-concepts/passby.js
@@ -0,0 +1,24 @@
+let num1 = 5;
+let num2 = 7;
+
+function multiply(a, b) {
+     a = 27;
+     const result = a * b;
+     return result;
+};
+
+// console.log(num1);
+const output = multiply(num1, num2);
+// console.log(output);
+
+let student1 = {name: 'Jalil', partner: 'Borsha'};
+let student2 = {name: 'raj', partner: 'anika'}
+
+function makeMonvie(couple1, couple2){
+     couple1.name = 'Ononto';
+     couple2.partner = 'Mim';
+}
+
+console.log(student1, student2);
+makeMonvie(student1, student2);
+console.log(student1, student2);

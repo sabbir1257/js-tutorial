7de09a889a13f287cff9738c8937ab8da918e6f1
sabbir1257 <masabbir1257@gmail.com>
2025-01-21 22:01:52 +0600

    Prop drilling, pass props in multi level

--- react-world-tour/src/components/CountryData/CountryData.jsx
+++ react-world-tour/src/components/CountryData/CountryData.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
-const CountryData = () => {
+const CountryData = ({ country, handleVisitedCountry, handleVisitedFlags }) => {
+     console.log('inside country data', country, handleVisitedCountry, handleVisitedFlags);
+     
      return (
           <div>
-               
+               <p><small>Country Data: {country.name.common}</small></p>
           </div>
      );
 };

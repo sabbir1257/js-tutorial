15feb6cf503fc8b3b3fe02c18ea394c852ec61ce
sabbir1257 <masabbir1257@gmail.com>
2025-01-25 01:30:39 +0600

    Display Saved cart from Local storage

--- memorable-water-bottle/src/components/Bottles/Bottles.jsx
+++ memorable-water-bottle/src/components/Bottles/Bottles.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import Bottle from '../Bottle/Bottle';
 import './Bottles.css'
 import { addToLS, getStoredCart } from '../../utilities/localstorage';
+import Cart from '../Cart/Cart';
 
 const Bottles = () => {
      const [bottles, setBottles] = useState([]);
@@ -18,7 +19,17 @@ const Bottles = () => {
           console.log('called the useEffect', bottles.length)
           if (bottles.length) {
                const storedCart = getStoredCart();
-               console.log(storedCart);
+               console.log(storedCart, bottles);
+               const savedCart = [];
+               for (const id of storedCart) {
+                    console.log(id);
+                    const bottle = bottles.find(bottle => bottle.id === id);
+                    if (bottle) {
+                         savedCart.push(bottle)
+                    }
+               }
+               console.log("saved cart", savedCart)
+               setCart(savedCart);
           }
      }, [bottles])
 
@@ -31,7 +42,7 @@ const Bottles = () => {
      return (
           <div>
                <h2>Bottles Available: {bottles.length}</h2>
-               <h4>Cart: {cart.length}</h4>
+               <Cart cart={cart}></Cart>
                <div className='bottle-container'>
                     {
                          bottles.map(bottle => <Bottle
